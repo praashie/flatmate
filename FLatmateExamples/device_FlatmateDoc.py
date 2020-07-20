3938952b666bfeaf9fcfc64edb29b2dffe30c8dc
Praash <praashie@gmail.com>
2020-07-21 02:07:39 +0300

    Add FLatmate Documentation hacker (not useful right now)

---
+++ FLatmateExamples/device_FlatmateDoc.py
@@ -0,0 +1,7 @@
+# name=Flatmate Documentation Hacker
+# url=https://forum.image-line.com/viewtopic.php?f=1994&t=227405
+
+import flatmate.doc
+
+def OnInit():
+    flatmate.doc.run("Z:/tmp/flatdoc/")
